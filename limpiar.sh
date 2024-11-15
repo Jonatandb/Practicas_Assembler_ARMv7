ffabfd35cdd55b4122003c2e0ce0700a38b98aa7
Jonatandb <jonatandb@gmail.com>
2024-11-15 10:56:05 -0300

    Agrego script de eliminación de archivos .o y .exe

---
+++ limpiar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Eliminando archivos .o y .exe..."
+rm -rf *.o *.exe # Borrado de todos los archivos .o y .exe
+
+# Para poder ejecutar este script, hay que darle permisos ejecutando:
+# chmod +x clean.sh
+
+
+# Para que este archivo se ejecute en Linux, la terminación de las lineas debe ser LF.
