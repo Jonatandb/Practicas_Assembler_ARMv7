f7847af6117a4dd00af4399dee6be5a9c08eeebf
Jonatandb <jonatandb@gmail.com>
2024-11-24 22:00:27 -0300

    Refactor: comentado de código línea a línea

--- 01_HolaMundo.s
+++ 01_HolaMundo.s
@@ -1,18 +1,17 @@
-.data
+.global main      // "main" será el punto de entrada
+
+.data             // Definición de "variables"
   cadena: .asciz "Hola Mundo!\n"
 
-.text
-
-.global main
-
+.text             // Comienza el código ejecutable (rutinas)
 main:
-  mov r0, #1
-  ldr r1, =cadena
-  mov r2, #12
-  mov r7, #4
-  swi #0
-  mov r0, #0
+  ldr r1, =cadena // En r1 va la dir. de la cadena a mostrar
+  mov r2, #12     // En r2 la longitud de la cadena
+  mov r0, #1      // Descriptor de archivo (stdout)
+  mov r7, #4      // syscall número 4: Escribir
+  swi 0           // swi: Software Interruption
 
 fin:
-  mov r7, #1
-  swi 0
+  mov r0, #0      // "Resultado" de ejecución del programa, cero -> OK
+  mov r7, #1      // syscall número 7: Fin del programa
+  swi 0           // swi: Software Interruption
